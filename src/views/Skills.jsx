ce6043e653ff8cd3e5c2f24b59d5ed2e25a82779
Singh Gaurav Ashokkumar <110033104+gaurav62472744@users.noreply.github.com>
2023-01-14 22:04:30 +0530

    added portfolio

--- src/views/Skills.jsx
+++ src/views/Skills.jsx
@@ -2,12 +2,13 @@ import React, { useContext } from "react";
 import { techStack } from "../constants";
 import { ThemeContext } from "../themeProvider";
 import { motion } from "framer-motion";
+import GitHubCalendar from "react-github-calendar";
 
 const Skills = () => {
   const theme = useContext(ThemeContext);
   const darkMode = theme.state.darkMode;
   return (
-    <div id="about" className={darkMode === true ? "bg-white" : "bg-gray-900"}>
+    <div id="skills" className={darkMode === true ? "bg-white" : "bg-gray-900"}>
       <div className="max-w-7xl mx-auto x-4 sm:px-6 lg:px-8 px-4 md:mt-0 pt-24 pb-12">
         <h2
           className={
@@ -65,7 +66,7 @@ const Skills = () => {
             </p>
           </motion.div> */}
           <motion.div>
-            <h4 className="mt-12 text-3xl font-semibold text-blue-500">
+            <h4 className="mt-20 text-3xl font-semibold text-blue-500">
               Technologies and Tools
             </h4>
             <p
@@ -102,6 +103,25 @@ const Skills = () => {
               </motion.div>
             ))}
           </motion.div>
+          <h2
+            className={
+              darkMode
+                ? "text-5xl font-bold px-4 md:px-0 text-center"
+                : "text-5xl font-bold px-4 md:px-0 text-center text-white"
+            }
+          >
+            Github Contribution
+          </h2>
+          <div
+            style={{
+              marginTop: "50px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <GitHubCalendar username="gaurav62472744" />
+          </div>
         </div>
       </div>
     </div>
